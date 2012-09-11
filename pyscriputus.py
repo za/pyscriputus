17e69f400855f2ddaaad1ed70b7235a65da7900d
Zaki Akhmad <zakiakhmad@gmail.com>
2012-09-11 12:34:33 +0700

    create new function

--- pyscriputus.py
+++ pyscriputus.py
@@ -4,21 +4,36 @@ import urllib
 import re
 
 # the html scrap function
+
+def extractURL(url):
+	sock = urllib.urlopen(url)
+	source = sock.read()
+	sock.close()
+	
+	url = re.findall(r'<a href=http://.+</a>', source)
+	print url
+
 def scrap(url): 
 	sock = urllib.urlopen(url)
 	htmlSource = sock.read()
 	sock.close()
-	print htmlSource
+	return htmlSource
 
-# the regex function():
-#def regex():
-
+# the find function():
+def find(pat, text):
+	match = re.search(pat,text)
+	if match: 
+		print match.group()
+	else:
+		print 'not found'
 
 # the main function
 def main():
-	scrap('http://localhost/za')
-#	regex()
-	
+#	scrap('http://localhost/za')
+#	print htmlSource
+#	find('http', htmlSource)	
+	extractURL('http://localhost/za')	
+
 # standard boilerplate to call the main() function.
 if __name__ == '__main__':
 	main()
